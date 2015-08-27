4f1c49aa4ea9f08a38ef9f32f49c16d6375dd559
cnring18 <charlesring@Charles-Rings-MacBook-Pro.local>
2015-08-27 11:15:58 -0400

    uninstall.js update

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-//After uninstall script to remove the uglify.js script from the users hooks/after_prepare directory
+//After uninstall script to remove the incrementBuildNum.js script from the users scripts/ directory
 
 var fs = require('fs'),
 	path = require('path'),
