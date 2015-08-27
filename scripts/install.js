36a1bd25e7d76c0368a4ccf121df4954d6944717
cnring18 <charlesring@Charles-Rings-MacBook-Pro.local>
2015-08-27 10:50:49 -0400

    Update

---
+++ scripts/install.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+//After install script - installs the cordova hook into scripts/ directory
+
+//We assume after post install, this script is run from its root directory
+
+//Before
+// ./proj
+//		/node_modules
+//			/cordova-build-increment
+//				incrementBuildNum.js
+//				/scripts
+//					install.js
+//					uninstall.js
+
+//After
+// ./proj
+//		/scripts
+//			incrementBuildNum.js
+//		/node_modules
+//			/cordova-build-increment
+//				incrementBuildNum.js
+
+var fs = require('fs'),
+	path = require('path'),
+	cwd = process.cwd(), //proj directory
+	scriptPath = __dirname, //node_modules/cordova-build-increment/scripts
+	writePath = path.join(cwd, '../../scripts');
+
+console.log(cwd, scriptPath, writePath);
+
+if(!fs.existsSync(writePath) {
+	console.log('Creating directory: ', writePath);
+	fs.mkdirSync(writePath);
+}	
+
+
+var buildIncrementPath = path.join(cwd, 'incrementBuildNum.js');
+
+var incrementFile = fs.readFileSync(buildIncrementPath);
+console.log('incrementFile: ', incrementFile)
+var incrementFilePath = path.join(writePath, 'incrementBuildNum.js')
+
+console.log('Creating increment hook: ', incrementFilePath)
+fs.writeFileSync(incrementFilePath, incrementFile); 
