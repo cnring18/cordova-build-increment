be77098e600e232d93d8e8ac9ea0d7c4b50846e1
cnring18 <charlesring@Charles-Rings-MacBook-Pro.local>
2015-08-27 11:14:38 -0400

    uninstall.js update

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -2,11 +2,11 @@
 
 //After uninstall script to remove the uglify.js script from the users hooks/after_prepare directory
 
-var fs = require('fs')
-var path = require('path')
-var cwd = process.cwd()
+var fs = require('fs'),
+	path = require('path'),
+	cwd = process.cwd();
 
-var uglifyJsPath = path.join(cwd, '../../', 'hooks', 'after_prepare', 'uglify.js')
+var buildIncrementPath = path.join(cwd, '../../scripts', 'incrementBuildNum.js');
 
-fs.unlink(uglifyJsPath)
-console.log('Removed: ', uglifyJsPath)
+fs.unlink(buildIncrementPath);
+console.log('Removed: ', buildIncrementPath);
